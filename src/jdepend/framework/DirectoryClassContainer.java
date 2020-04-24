76b0060a7fce533fdaf8d2096cb8ffc55a52c6c7
Doc Norton <doc@docondev.com>
2020-04-23 20:34:37 -0400

    Introduce Directory Class Container

---
+++ src/jdepend/framework/DirectoryClassContainer.java
@@ -0,0 +1,12 @@
+package jdepend.framework;
+
+import java.io.IOException;
+
+public class DirectoryClassContainer extends ClassContainerBase {
+    public DirectoryClassContainer(String source) throws IOException {
+        super(source);
+    }
+
+    @Override
+    protected boolean isNotAContainer() { return !location.isDirectory(); }
+}
