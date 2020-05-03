554f25ccaf12fab677d475ac11442a70ce4c2120
Doc Norton <doc@docondev.com>
2020-05-03 06:27:52 -0400

    Remove comments

--- src/jdepend/framework/JavaClassBuilder.java
+++ src/jdepend/framework/JavaClassBuilder.java
@@ -54,9 +54,9 @@ public class JavaClassBuilder {
 
         Collection classes = new ArrayList();
 
-        for (File file : fileManager.extractFiles()) {              // returns archives and class files - this is broken.
+        for (File file : fileManager.extractFiles()) {
             try {
-                classes.addAll(buildClasses(file));                 // process class files AND
+                classes.addAll(buildClasses(file));
             } catch (IOException ioe) {
                 System.err.println("\n" + ioe.getMessage());
             }
