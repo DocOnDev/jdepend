0846c9640208d4329c77904b0c73a558c23ac246
Doc Norton <doc@docondev.com>
2020-06-12 14:22:19 -0400

    Move test into ClassContainers

--- test/jdepend/framework/ClassContainersTest.java
+++ test/jdepend/framework/ClassContainersTest.java
@@ -78,4 +78,25 @@ public class ClassContainersTest extends JDependTestCase {
         }
     }
 
+    public void testThrowsExceptionWhenFileNotFound() {
+        String missingFilePath = getTestDataDir() + "non-existent.file";
+
+        try {
+            classContainers.buildClasses(new File(missingFilePath));
+            fail("Should raise IOException");
+        } catch (IOException e) {
+            assertEquals("File is not a valid .class, .jar, .war, or .zip file: " + missingFilePath, e.getMessage());
+        }
+    }
+
+    public void testThrowsExceptionWhenFileNotValid() {
+        try {
+            classContainers.buildClasses(new File(getTestDataDir() + "bad.jar"));
+            fail("Should raise IOException");
+        } catch (IOException e) {
+            assertEquals("error in opening zip file", e.getMessage());
+        }
+    }
+
+
 }
--- test/jdepend/framework/JavaClassBuilderTest.java
+++ test/jdepend/framework/JavaClassBuilderTest.java
@@ -14,23 +14,4 @@ public class JavaClassBuilderTest extends JDependTestCase{
         super.tearDown();
     }
 
-    public void testThrowsExceptionWhenFileNotFound() {
-        String missingFilePath = getTestDataDir() + "non-existent.file";
-
-        try {
-            classContainers.buildClasses(new File(missingFilePath));
-            fail("Should raise IOException");
-        } catch (IOException e) {
-            assertEquals("File is not a valid .class, .jar, .war, or .zip file: " + missingFilePath, e.getMessage());
-        }
-    }
-
-    public void testThrowsExceptionWhenFileNotValid() {
-        try {
-            classContainers.buildClasses(new File(getTestDataDir() + "bad.jar"));
-            fail("Should raise IOException");
-        } catch (IOException e) {
-            assertEquals("error in opening zip file", e.getMessage());
-        }
-    }
 }
